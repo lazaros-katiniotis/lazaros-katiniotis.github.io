0af3e8afb0ce77ee0fcf4000d61a3d71bc46b140
Lazaros Katiniotis <thrashsheep@gmail.com>
2021-03-14 00:13:23 +0100

    Readjusted alignment and responsiveness of certain elements.

--- create_carousel_items.js
+++ create_carousel_items.js
@@ -1,4 +1,7 @@
 const carousel_items = [];
+const project_images = new Map();
+project_images['RouteApplication'] = './resources/projects/routeapplication.png';
+project_images['Steves Lab'] = './resources/projects/steves_lab.png';
 
 class Project {
     constructor(title, description, url, homepage, language) {
@@ -7,6 +10,19 @@ class Project {
       this.url = url;
       this.homepage = homepage;
       this.language = language;
+      this.img_url = project_images[title];
+      if (!this.img_url) {
+        this.img_url = './resources/projects/no_img.png';
+      }
+    }
+
+    printProject() {
+        console.log(`title: ${this.title}`);
+        console.log(`description: ${this.description}`);
+        console.log(`url: ${this.url}`);
+        console.log(`homepage: ${this.homepage}`);
+        console.log(`language: ${this.language}`);
+        console.log(`img_url: ${this.img_url}`);
     }
 }
 
@@ -14,13 +30,16 @@ function repoNameToTitle(name) {
     return name.replace('_', ' ');
 }
 
-getProjects().then(projects => {
+getProjects()
+.then(projects => {
     projects.forEach(project => {
         const item = new Project(repoNameToTitle(project.name), project.description, project.html_url, project.homepage, project.language);
         carousel_items.push(item);
-        console.log(item);
+        item.printProject();
     });
 });
+
+
 
 
 
