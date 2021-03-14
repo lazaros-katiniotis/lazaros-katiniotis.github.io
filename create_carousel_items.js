23bba1291022f346379631745a73892925b721ab
Lazaros Katiniotis <thrashsheep@gmail.com>
2021-03-14 12:08:05 +0100

    Carousel items are now created from script. Adjusted width size for certain elements to keep responsiveness

--- create_carousel_items.js
+++ create_carousel_items.js
@@ -35,10 +35,51 @@ getProjects()
     projects.forEach(project => {
         const item = new Project(repoNameToTitle(project.name), project.description, project.html_url, project.homepage, project.language);
         carousel_items.push(item);
-        item.printProject();
+        createItem(item);
+        //item.printProject();
     });
 });
+/* <div class="carousel-item colored-background">
+    <a href="./"><img src="./resources/projects/routeapplication.png" alt=""></a>
+    <div class="carousel-caption">
+        <h3>title</h3>
+        <p>description</p>
+        <p>language</p>
+    </div>
+</div> */
+const items_container = document.getElementById('items-container');
 
+function createItem(project) {
+    const card = document.createElement("div"),
+        link = document.createElement("a"),
+        image = document.createElement("img"),
+        caption = document.createElement("div"),
+        title = document.createElement("h3"),
+        description = document.createElement("p"),
+        language = document.createElement("p");
+    
+    console.log(project);
+    
+    image.setAttribute("src", project.img_url);
+    link.setAttribute("href", project.homepage);
+    caption.classList.add('carousel-caption');
+    card.classList.add('carousel-item', 'colored-background');
+
+    title.textContent = project.title;
+    description.textContent = project.description;
+    language.textContent = project.language;
+    
+    caption.appendChild(title);
+    caption.appendChild(description);
+    caption.appendChild(language);
+    
+    link.appendChild(image);
+    
+    card.appendChild(link);
+    card.appendChild(caption);
+    
+    items_container.appendChild(card);
+}
 
 
 
