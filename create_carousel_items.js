fa51f67693b75bf95181b1a17d3abd1e4753cdb6
Lazaros Katiniotis <thrashsheep@gmail.com>
2021-03-16 00:06:57 +0100

    Carousel items no longer have an img element. Project image is added to carousel item as background image.

--- create_carousel_items.js
+++ create_carousel_items.js
@@ -30,38 +30,35 @@ function repoNameToTitle(name) {
     return name.replace('_', ' ');
 }
 
-getProjects()
-.then(projects => {
-    projects.forEach(project => {
-        const item = new Project(repoNameToTitle(project.name), project.description, project.html_url, project.homepage, project.language);
-        carousel_items.push(item);
-        createItem(item);
-        //item.printProject();
+const carousel = document.getElementById('projects-carousel');
+
+const createCarouselItems = async () => {
+    await getProjects()
+    .then(projects => {
+        projects.forEach(project => {
+            const item = new Project(repoNameToTitle(project.name), project.description, project.html_url, project.homepage, project.language);
+            carousel_items.push(item);
+            //item.printProject();
+        });
     });
-});
-/* <div class="carousel-item colored-background">
-    <a href="./"><img src="./resources/projects/routeapplication.png" alt=""></a>
-    <div class="carousel-caption">
-        <h3>title</h3>
-        <p>description</p>
-        <p>language</p>
-    </div>
-</div> */
-const items_container = document.getElementById('items-container');
+    createItem(carousel_items[1]);
+}
 
 function createItem(project) {
     const card = document.createElement("div"),
         link = document.createElement("a"),
-        image = document.createElement("img"),
+        //image = document.createElement("img"),
         caption = document.createElement("div"),
         title = document.createElement("h3"),
         description = document.createElement("p"),
         language = document.createElement("p");
     
-    console.log(project);
+    //console.log(project);
     
-    image.setAttribute("src", project.img_url);
+    card.style.backgroundImage = `url('${project.img_url}')`;
+    //image.setAttribute("src", project.img_url);
     link.setAttribute("href", project.homepage);
+    link.setAttribute("target", '_blank');
     caption.classList.add('carousel-caption');
     card.classList.add('carousel-item', 'colored-background');
 
@@ -72,18 +69,13 @@ function createItem(project) {
     caption.appendChild(title);
     caption.appendChild(description);
     caption.appendChild(language);
-    
-    link.appendChild(image);
-    
+
+    link.appendChild(caption);
+        
+    //card.appendChild(image);
     card.appendChild(link);
-    card.appendChild(caption);
     
-    items_container.appendChild(card);
+    carousel.appendChild(card);
 }
 
-
-
-
-
-
-
+createCarouselItems();
