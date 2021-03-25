646367385103321865f91f65dd9b9ce0c34ec77a
Lazaros Katiniotis <thrashsheep@gmail.com>
2021-03-25 15:55:23 +0100

    Creation of project carousel items is now encapsulated in the correct file

--- resources/scripts/carousel.js
+++ resources/scripts/carousel.js
@@ -7,34 +7,6 @@ const nextCardPreview = document.getElementById('next-preview');
 const carouselItems = [];
 const cardsContainer = document.getElementById('cards-container');
 
-const createCarouselItems = async (carousel) => {
-    await getProjects()
-    .then(projects => {
-        projects.forEach(project => {
-            const item = new Project(project.name, project.description, project.html_url, project.homepage, project.language);
-            carouselItems.push(item);
-        });
-    });
-
-    let i = 0;
-    if (carouselItems.length === 1) {
-        carousel.createItem(carouselItems[i++], "view-item");
-    }
-    else if (carouselItems.length === 2) {
-        carousel.createItem(carouselItems[i++], "prev-item");
-        carousel.createItem(carouselItems[i++], "view-item");
-    }
-    else if (carouselItems.length === 3) {
-        carousel.createItem(carouselItems[i++], "prev-item");
-        carousel.createItem(carouselItems[i++], "view-item");
-        carousel.createItem(carouselItems[i++], "next-item");
-    }
-
-    for (i; i < carouselItems.length; i++) {
-        carousel.createItem(carouselItems[i]);
-    }
-}
-
 class Carousel {
 
     getPreviousCard(card) {
